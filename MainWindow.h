2103a0030d20e8a783a9aa84312225deaa862a89
kieung kim <europaa@gmail.com>
2016-02-14 02:38:37 +0900

    minor beautification

--- MainWindow.h
+++ MainWindow.h
@@ -1,22 +1,26 @@
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
 
+
 #include <QMainWindow>
+
 
 namespace Ui {
 class MainWindow;
 }
+
 
 class MainWindow : public QMainWindow
 {
     Q_OBJECT
 
 public:
-    explicit MainWindow(QWidget *parent = 0);
-    ~MainWindow();
+    explicit        MainWindow      (QWidget *parent = 0);
+                    ~MainWindow     ();
 
 private:
-    Ui::MainWindow *ui;
+    Ui::MainWindow* ui;
 };
+
 
 #endif // MAINWINDOW_H
